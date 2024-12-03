f99a07d34ce8c3324e24e8d0951cf9da326d2ccc
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2024-12-03 15:52:03 +0100

    javascript + css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 // *** LIGHTBOX ***
+const listThumbnail = document.querySelector("[data-full-img]");  //retrouve toute les miniatures
+const lightboxImg = document.querySelector('#lightbox > img');
+const lightbox = document.querySelector('#lightbox');
+
+listThumbnail.forEach((thumbnail) => {
+	thumbnail.addEventListener('click', (evt) => {
+		lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.showModal();
+	});
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.close();
+});
+

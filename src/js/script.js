c515803feb7f9b4d24322c20251b7efb57caacbd
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2024-12-03 16:10:13 +0100

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,36 @@
 // *** LIGHTBOX ***
-const listThumbnail = document.querySelector("[data-full-img]");  //retrouve toute les miniatures
+/*const lightbox = document.querySelector('#lightbox');
+const listThumbnail = document.querySelectorAll("[data-full-img]");
 const lightboxImg = document.querySelector('#lightbox > img');
 
 listThumbnail.forEach((thumbnail) => {
-	thumbnail.addEventListener('click', (evt) => {
-		lightboxImg.src = thumbnail.dataset.fullImg;
+    thumbnail.addEventListener('click', (evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
         lightbox.showModal();
-	});
+    })
 });
 
 lightbox.addEventListener('click', () => {
-  lightbox.close();
+    lightbox.close();
+});
+*/
+// *** LIGHTBOX ***
+const lightbox = document.querySelector('#lightbox');
+const listThumbnail = document.querySelectorAll("[data-full-img]");
+const lightboxImg = document.querySelector('#lightbox > img');
+
+listThumbnail.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', (evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.classList.add('show');
+        lightbox.classList.remove('hide');
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.add('hide');
+    lightbox.classList.remove('show');
+    setTimeout(() => {
+        lightbox.close();
+    }, 500); // Durée de l'animation
 });
